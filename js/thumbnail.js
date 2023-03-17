b85cb8fd1bd0967b147ec1966c6a1cc2eac15f4b
ваше имя <vitamin.vm0@gmail.com>
2023-03-17 15:46:59 +0300

    Отрисовывает картинки

---
+++ js/thumbnail.js
@@ -0,0 +1,25 @@
+const thumbnailTemplate = document.querySelector('#picture').
+  content.querySelector('.picture');
+const container = document.querySelector('.picture');
+
+
+const createThumbnail = ({url, likes, comments}) => {
+  const thumbanail = thumbnailTemplate.cloneNode(true);
+
+  thumbanail.querySelector('.picture__img').src = url;
+  thumbanail.querySelector('.picture__likes').textContent = likes;
+  thumbanail.querySelector('.picture__comments').textContent = comments;
+
+  return thumbanail;
+
+const renderThumbnails = (pictures) => {
+  const fragment = document.createDocumentFragment();
+  pictures.forEach((picture) => {
+    const thumbanail = createThumbnail(picture);
+    fragment.append(thumbanail);
+  });
+
+  container.append(fragment);
+};
+
+export {renderThumbnails};
